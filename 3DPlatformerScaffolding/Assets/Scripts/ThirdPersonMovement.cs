6f2bc3e4b4ddc127b69ce4c6e377508cc184fa9e
nguyenhanson1 <nguyenhanson1@gmail.com>
2020-01-07 16:19:47 -0800

    Added Free Persepective for Player

--- 3DPlatformerScaffolding/Assets/Scripts/ThirdPersonMovement.cs
+++ 3DPlatformerScaffolding/Assets/Scripts/ThirdPersonMovement.cs
@@ -18,22 +18,20 @@ public class ThirdPersonMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayerMovement();
+
+        
+    }
+    void PlayerMovement()
+    {
         /** Horrizontal Input value. Return 1 for right, -1 for left, and 0 if nothing*/
         Horizontal = Input.GetAxis("Horizontal");
 
         /**Vertical Input value. Return 1 for forward, -1 for backward, and 0 if nothing*/
         Vertical = Input.GetAxis("Vertical");
 
-        Vector3 right = this.transform.right * Horizontal * speed;
-        rigid.velocity = new Vector3(Mathf.Clamp(rigid.velocity.x + right.x, -speed, speed),
-                                     rigid.velocity.y,
-                                     Mathf.Clamp(rigid.velocity.z + right.z, -speed, speed));
-        Vector3 forward = this.transform.forward * Vertical * speed;
-        rigid.velocity = new Vector3(Mathf.Clamp(rigid.velocity.x + forward.x, -speed, speed),
-                                     rigid.velocity.y,
-                                     Mathf.Clamp(rigid.velocity.z + forward.z, -speed, speed));
-        
+        Vector3 playerMovement = new Vector3(Horizontal, 0f, Vertical) * speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
 
-        
     }
 }
